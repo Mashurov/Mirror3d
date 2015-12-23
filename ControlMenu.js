fc29c202711743b1d6cc280936dfe4d9034944f9
Mashurov <Mashurov.vadimka@gmail.com>
2015-12-23 14:28:35 +0200

    Scripts from the unity project

---
+++ ControlMenu.js
@@ -0,0 +1,44 @@
+﻿var isNewGameButton = false;
+var isLevelButton = false;
+var isLevel2Button = false;
+var isMenuButton = false;
+var isQuitButton = false;
+var camera1 : Camera;
+var camera2 : Camera;
+
+function OnMouseEnter()
+{
+    GetComponent.<Renderer>().material.color = Color.red;
+}
+
+function OnMouseExit()
+{
+    GetComponent.<Renderer>().material.color = Color.white;
+}
+
+function OnMouseUp()
+{
+    if (isQuitButton)
+    {
+        Application.Quit();
+    }
+    else if (isLevelButton)
+    {
+        camera1.enabled = false;
+        camera2.enabled = true;
+    }
+    else if (isLevel2Button)
+    {
+        Application.LoadLevel (2);
+    }
+    else if (isMenuButton)
+    {
+        Application.LoadLevel (0);
+    }
+    else if (isNewGameButton)
+    {
+        Application.LoadLevel (1);
+    }
+
+}
+
---
+++ NextLevel.js
@@ -0,0 +1,25 @@
+﻿public var i = 0;
+private var inRange: boolean = false;
+
+ 
+function OnTriggerEnter(other:Collider) {
+    if (other.tag == "Player") {
+        inRange =  true;
+    }
+}
+ 
+    function OnTriggerExit(other:Collider) {
+        if (other.tag == "Player") {
+            inRange =  false;
+        }
+    }
+ 
+        function OnGUI() {
+            if(inRange) {
+                GUI.Label (Rect (960, 512, 1000, 50),"Congratulations : Level Completed!");
+                Application.LoadLevel(2);
+                                                                           
+             
+            }
+        }
+
